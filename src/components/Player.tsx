7528812690d037bd14363a26e38ecc8c0e361ee6
grzesikb <grzesikb@wp.pl>
2023-06-01 18:11:13 +0200

    add new build

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
-import { IVideo } from '../App';
+import { IVideo } from './App';
 
 const Player = (props: IVideo) => {
   return <ReactPlayer url={props.link} controls width="100%" height="auto" />;
