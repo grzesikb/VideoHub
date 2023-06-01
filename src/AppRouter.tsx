1b167614afd164192363ba6999ab00dd51672ce6
grzesikb <grzesikb@wp.pl>
2023-06-01 20:41:22 +0200

    test basename

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -2,12 +2,15 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './components/App';
 
 const AppRouter = () => {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <App />,
-    },
-  ]);
+  const router = createBrowserRouter(
+    [
+      {
+        path: '/',
+        element: <App />,
+      },
+    ],
+    { basename: '/videohub' },
+  );
   return <RouterProvider router={router} />;
 };
 
